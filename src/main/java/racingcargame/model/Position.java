97d46142f7b4beddd91df1b151a53aaa52643f61
jaejae-yoo <wotj102@gmail.com>
2022-02-21 22:22:36 +0900

    refactor: Position 객체 생성 검증

--- src/main/java/racingcargame/model/Position.java
+++ src/main/java/racingcargame/model/Position.java
@@ -3,10 +3,18 @@ package racingcargame.model;
 import java.util.Objects;
 
 public class Position {
+    private static final String NEGATIVE_ERROR = "[error] 양수를 입력해주세요.";
     private int position;
 
     public Position(int position) {
+        validateNumber(position);
         this.position = position;
+    }
+
+    private void validateNumber(int position) {
+        if (position < 0) {
+            throw new IllegalArgumentException(NEGATIVE_ERROR);
+        }
     }
 
     public void addPosition() {
